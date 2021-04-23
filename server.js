4e3f21fba11593cf4e929bc1c76e52379c85a463
Miss <emesquitta73@gmail.com>
2021-04-23 20:38:01 +0100

    completed level 2

--- server.js
+++ server.js
@@ -7,34 +7,45 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
 
 //load the quotes JSON
- const quotes = require("./quotes.json");
+const quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", (req, res) =>
-  res.send("<h1>Elisha's Quote Server!!  Ask me for quotes or random quotes</h1>"));
+  res.send(
+    "<h1>Elisha's Quote Server!!  Ask me for quotes or random quotes</h1>"
+  )
+);
 
+app.get("/quotes", (req, res) => res.json(quotes));
 
-  app.get("/quotes", (req, res)=> 
-    res.json(quotes));
-  
-  
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
-
-app.get("/quotes/random", (req, res) =>{
-
-  const randomQuote = pickFromArray(quotes)
-  res.json(randomQuote)
+app.get("/quotes/random", (req, res) => {
+  const randomQuote = pickFromArray(quotes);
+  res.json(randomQuote);
 
   function pickFromArray(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
   }
 });
+
+app.get("/quotes/search", (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  const searchResult = quotes.filter((quotes1) => {
+    if (
+      quotes1.quote.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      quotes1.author.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+      return quotes1;
+    })
+
+    
+  res.json(searchResult)
+    
+    
+});
+
 
 
 //Start our server so that it listens for HTTP requests!
